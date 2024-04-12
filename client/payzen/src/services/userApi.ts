12589683fe7421bc45f194ec6942b55d21c0e0ff
Ruslan Bayramov <ruslannbayramovvv@gmail.com>
2024-04-13 01:26:08 +0400

    fifth commit

--- client/payzen/src/services/userApi.ts
+++ client/payzen/src/services/userApi.ts
@@ -1,15 +1,12 @@
 export async function login(email: string, password: string) {
-  const res = await fetch(
-    "https://payzen-2vex.onrender.com/api/v1/users/login",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: email.trim(),
-        password: password.trim(),
-      }),
-    }
-  );
+  const res = await fetch("http://127.0.0.1:8080/api/v1/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email.trim(),
+      password: password.trim(),
+    }),
+  });
 
   const data = await res.json();
   return data;
@@ -22,20 +19,17 @@ export async function signup(
   password: string,
   confirmPassword: string
 ) {
-  const res = await fetch(
-    "https://payzen-2vex.onrender.com/api/v1/users/signup",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: name.trim(),
-        surname: surname.trim(),
-        email: email.trim(),
-        password: password.trim(),
-        confirmPassword: confirmPassword.trim(),
-      }),
-    }
-  );
+  const res = await fetch("http://127.0.0.1:8080/api/v1/users/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: name.trim(),
+      surname: surname.trim(),
+      email: email.trim(),
+      password: password.trim(),
+      confirmPassword: confirmPassword.trim(),
+    }),
+  });
 
   const data = await res.json();
   return data;
